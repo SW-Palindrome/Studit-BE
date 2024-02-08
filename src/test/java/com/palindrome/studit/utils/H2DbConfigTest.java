dcd73db53bfd9434655d172e11a4317c44e53454
Yujeong Jeong <jjungyu1282@gmail.com>
2024-02-08 22:44:23 +0900

    Test: add h2 db connection test

--- src/test/java/com/palindrome/studit/utils/H2DbConfigTest.java
+++ src/test/java/com/palindrome/studit/utils/H2DbConfigTest.java
@@ -1,2 +1,28 @@
-package com.palindrome.studit.utils;public class H2DbConfigTest {
+package com.palindrome.studit.utils;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class H2DbConfigTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    @DisplayName("h2 db 연결 테스트")
+    void DbConnectionTest() throws SQLException {
+        Connection conn = dataSource.getConnection();
+        assertThat(conn).isNotNull();
+    }
 }
