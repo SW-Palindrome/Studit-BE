56bb404ab6ba9077ad6a7e7bd597dcb22b7f0c7f
Yujeong Jeong <jjungyu1282@gmail.com>
2024-02-08 22:40:11 +0900

    Feat: add user repository

--- src/main/java/com/palindrome/studit/domain/user/dao/UserRepository.java
+++ src/main/java/com/palindrome/studit/domain/user/dao/UserRepository.java
@@ -1,2 +1,8 @@
-package com.palindrome.studit.domain.user.dao;public interface UserRepository {
+package com.palindrome.studit.domain.user.dao;
+
+import com.palindrome.studit.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByNickname(String nickname);
 }
