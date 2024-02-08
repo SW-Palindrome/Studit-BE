2f8985cb470b990c51a09b50c0bb8dccfbf9e8c3
Yujeong Jeong <jjungyu1282@gmail.com>
2024-02-08 22:45:13 +0900

    Test: add user repository test

--- src/test/java/com/palindrome/studit/user/repository/UserRepositoryTest.java
+++ src/test/java/com/palindrome/studit/user/repository/UserRepositoryTest.java
@@ -1,2 +1,63 @@
-package com.palindrome.studit.user.repository;public class UserRepositoryTest {
+package com.palindrome.studit.user.repository;
+
+import com.palindrome.studit.domain.user.dao.UserRepository;
+import com.palindrome.studit.domain.user.entity.User;
+import com.palindrome.studit.domain.user.entity.UserRoleType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Test
+    @DisplayName("유저 생성 테스트")
+    void createUserTest() {
+        //Given
+        User user = User.builder()
+                .email("swm.palindrome@gmail.com")
+                .nickname("palindrome")
+                .profileImage("https://palworld.shwa.space/assets/PalIcon/T_PinkCat_icon_normal.png")
+                .agreeTOS(true)
+                .agreePICU(true)
+                .roleType(UserRoleType.ADMIN)
+                .build();
+
+        userRepository.save(user);
+
+        //When
+        User savedUser = userRepository.findByNickname("palindrome");
+
+        //Then
+        assertThat(savedUser.getEmail()).isEqualTo("swm.palindrome@gmail.com");
+        assertThat(savedUser.getRoleType()).isEqualTo(UserRoleType.ADMIN);
+        assertThat(savedUser.getCreatedDate()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("테스트 데이터 조회")
+    void findAllUsersTest() {
+        //Given: data.sql
+
+        //When
+        List<User> users = userRepository.findAll();
+
+        //Then
+        assertThat(users).isNotEmpty();
+        assertThat(users.get(0).getNickname()).isEqualTo("USER1");
+        assertThat(users.get(1).getNickname()).isEqualTo("USER2");
+    }
 }
