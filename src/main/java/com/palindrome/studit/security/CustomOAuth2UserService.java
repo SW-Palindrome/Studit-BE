e681c06111600fd807b57dce4be43560bac24c89
jshong0907 <jshong0907@gmail.com>
2024-02-14 19:58:01 +0900

    Refactor: extract OAuth provider type mapping to a map

--- src/main/java/com/palindrome/studit/security/CustomOAuth2UserService.java
+++ src/main/java/com/palindrome/studit/security/CustomOAuth2UserService.java
@@ -11,23 +11,21 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final AuthService authService;
+    private static final Map<String, OAuthProviderType> oAuthProviderTypeMap = Map.of(
+        "github", OAuthProviderType.GITHUB
+    );
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         ClientRegistration clientRegistration = userRequest.getClientRegistration();
-        OAuthProviderType oAuthProviderType;
-        switch (userRequest.getClientRegistration().getRegistrationId()) {
-            case "github":
-                oAuthProviderType = OAuthProviderType.GITHUB;
-                break;
-            default:
-                throw new OAuth2AuthenticationException("client id not match");
-        }
+        OAuthProviderType oAuthProviderType = oAuthProviderTypeMap.get(clientRegistration.getRegistrationId());
         User user = authService.createUser(oAuth2User.getAttribute("email"), oAuthProviderType, clientRegistration.getClientId());
         return new CustomOAuth2User(user, oAuth2User.getAttributes());
     }
