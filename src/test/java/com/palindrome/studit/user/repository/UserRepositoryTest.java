c33a42978e5065813c7e88b1958d269be456ea92
Yujeong Jeong <jjungyu1282@gmail.com>
2024-02-26 16:14:22 +0900

    Refactor: remove @AutoConfigureTestDatabase annotation

--- src/test/java/com/palindrome/studit/user/repository/UserRepositoryTest.java
+++ src/test/java/com/palindrome/studit/user/repository/UserRepositoryTest.java
@@ -6,8 +6,6 @@ import com.palindrome.studit.domain.user.entity.UserRoleType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -17,7 +15,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2, replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
 
     @Autowired
