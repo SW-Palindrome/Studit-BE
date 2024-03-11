067b55f0cf099665ca4442670eaae62521221d03
jshong0907 <jshong0907@gmail.com>
2024-03-11 14:44:09 +0900

    Feat: study enroll api

--- src/main/java/com/palindrome/studit/domain/study/api/StudyController.java
+++ src/main/java/com/palindrome/studit/domain/study/api/StudyController.java
@@ -4,6 +4,8 @@ import com.palindrome.studit.domain.study.application.StudyService;
 import com.palindrome.studit.domain.study.domain.Study;
 import com.palindrome.studit.domain.study.dto.CreateStudyDTO;
 import com.palindrome.studit.domain.study.dto.StudyResponseDTO;
+import com.palindrome.studit.domain.study.exception.DuplicatedStudyEnrollmentException;
+import com.palindrome.studit.domain.user.exception.InvalidTokenException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -34,5 +36,16 @@ public class StudyController {
     public Page<StudyResponseDTO> listMyStudies(Authentication authentication, @RequestParam(value = "page", defaultValue = "0") int page) {
         Page<Study> studies = studyService.getListByUser(page, Long.parseLong(authentication.getName()));
         return StudyResponseDTO.toDTOPage(studies);
+    }
+
+    @PostMapping("/{studyId}/enroll")
+    public ResponseEntity<Object> enrollStudy(Authentication authentication, @PathVariable("studyId") Long studyId) throws DuplicatedStudyEnrollmentException {
+        studyService.enroll(Long.parseLong(authentication.getName()), studyId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @ExceptionHandler({ DuplicatedStudyEnrollmentException.class })
+    public ResponseEntity<Object> duplicatedStudyEnrollmentException(final DuplicatedStudyEnrollmentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }
