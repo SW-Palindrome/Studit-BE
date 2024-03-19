4ce3c738b0b704c1aef3837eead9e5c9056f921a
jshong0907 <jshong0907@gmail.com>
2024-03-19 23:30:15 +0900

    Feat: save github pr logs

--- src/main/java/com/palindrome/studit/domain/study/application/GithubMissionService.java
+++ src/main/java/com/palindrome/studit/domain/study/application/GithubMissionService.java
@@ -2,23 +2,32 @@ package com.palindrome.studit.domain.study.application;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.palindrome.studit.domain.study.dao.StudyEnrollmentRepository;
+import com.palindrome.studit.domain.study.dao.StudyLogRepository;
 import com.palindrome.studit.domain.study.domain.MissionType;
 import com.palindrome.studit.domain.study.domain.StudyEnrollment;
+import com.palindrome.studit.domain.study.domain.StudyLog;
 import com.palindrome.studit.domain.study.domain.StudyStatus;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class GithubMissionService {
     private final StudyEnrollmentRepository studyEnrollmentRepository;
+    private final StudyLogRepository studyLogRepository;
 
-    @Scheduled(cron = "0 */1 * ? * *")
-    public void collectCommits() {
+    @Scheduled(cron = "0 */10 * ? * *")
+    public void savePullRequest() {
         List<StudyEnrollment> studyEnrollments = studyEnrollmentRepository.findAllByStudy_Mission_MissionTypeAndStudy_Status(MissionType.GITHUB, StudyStatus.IN_PROGRESS);
         RestTemplate restTemplate = new RestTemplate();
 
@@ -28,8 +37,6 @@ public class GithubMissionService {
             if (missionUrl == null || missionUrl.isBlank()) {
                 continue;
             }
-
-            System.out.println(missionUrl);
 
             String[] parts = missionUrl.split("/");
 
@@ -44,7 +51,20 @@ public class GithubMissionService {
             }
 
             for (JsonNode repoInfo : result) {
-                System.out.println(repoInfo.get("html_url"));
+                String completedMissionUrl = repoInfo.get("html_url").asText();
+                LocalDateTime completedAt = LocalDateTime.parse(repoInfo.get("merged_at").asText(), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'"));
+
+                StudyLog studyLog = StudyLog.builder()
+                        .studyEnrollment(studyEnrollment)
+                        .completedMissionUrl(completedMissionUrl)
+                        .completedAt(completedAt)
+                        .build();
+
+                try {
+                    studyLogRepository.save(studyLog);
+                } catch (ConstraintViolationException | DataIntegrityViolationException e) {
+                    log.warn(completedMissionUrl + "is ignored!");
+                }
             }
         }
     }
