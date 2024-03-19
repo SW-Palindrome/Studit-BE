00d828f8bad85960b994d8cba12e8c43183fad84
Yujeong Jeong <jjungyu1282@gmail.com>
2024-03-20 01:17:52 +0900

    Feat: add find by user and study query method

--- src/main/java/com/palindrome/studit/domain/study/dao/StudyEnrollmentRepository.java
+++ src/main/java/com/palindrome/studit/domain/study/dao/StudyEnrollmentRepository.java
@@ -1,9 +1,14 @@
 package com.palindrome.studit.domain.study.dao;
 
+import com.palindrome.studit.domain.study.domain.Study;
 import com.palindrome.studit.domain.study.domain.StudyEnrollment;
+import com.palindrome.studit.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StudyEnrollmentRepository extends JpaRepository<StudyEnrollment, Long> {
+    Optional<StudyEnrollment> findByUserAndStudy(User user, Study study);
 }
