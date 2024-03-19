4bed3e28c9653c34e16b84bcb3b6a82413d059aa
Yujeong Jeong <jjungyu1282@gmail.com>
2024-03-20 01:22:43 +0900

    Feat: add error response

--- src/main/java/com/palindrome/studit/global/error/ErrorResponse.java
+++ src/main/java/com/palindrome/studit/global/error/ErrorResponse.java
@@ -1,2 +1,14 @@
-package com.palindrome.studit.global.error;public class ErrorResponse {
+package com.palindrome.studit.global.error;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+    private HttpStatus status;
+
 }
