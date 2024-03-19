bc172e7c44469114357b8d10e6e23de3dfb45342
Yujeong Jeong <jjungyu1282@gmail.com>
2024-03-20 01:18:27 +0900

    Feat: add mission url request dto

--- src/main/java/com/palindrome/studit/domain/study/dto/MissionUrlRequestDTO.java
+++ src/main/java/com/palindrome/studit/domain/study/dto/MissionUrlRequestDTO.java
@@ -1,2 +1,10 @@
-package com.palindrome.studit.domain.study.dto;public class MissionUrlRequestDTO {
+package com.palindrome.studit.domain.study.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MissionUrlRequestDTO {
+    private String missionUrl;
 }
