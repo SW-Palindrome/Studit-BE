b3b648d371d56fb002b3ee9945fc6ae915dcb615
Yujeong Jeong <jjungyu1282@gmail.com>
2024-03-26 00:15:59 +0900

    Chore: add builder, allargsconstructor annotation

--- src/main/java/com/palindrome/studit/domain/study/dto/MissionUrlRequestDTO.java
+++ src/main/java/com/palindrome/studit/domain/study/dto/MissionUrlRequestDTO.java
@@ -1,10 +1,14 @@
 package com.palindrome.studit.domain.study.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class MissionUrlRequestDTO {
     private String missionUrl;
 }
