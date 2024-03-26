b7453008c4ea5e7873a6949fd945980f051b6423
jshong0907 <jshong0907@gmail.com>
2024-03-27 01:19:54 +0900

    Feat: add mission list api

---
+++ src/main/java/com/palindrome/studit/domain/mission/api/MissionController.java
@@ -0,0 +1,27 @@
+package com.palindrome.studit.domain.mission.api;
+
+import com.palindrome.studit.domain.mission.application.MissionService;
+import com.palindrome.studit.domain.mission.domain.MissionState;
+import com.palindrome.studit.domain.mission.dto.WeeklyMissionStateResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/missions")
+public class MissionController {
+    private final MissionService missionService;
+
+    @GetMapping("/my/weekly")
+    public Page<WeeklyMissionStateResponseDTO> listMyWeeklyMissionStates(Authentication authentication, @RequestParam(value = "page", defaultValue = "0") int page) {
+        Page<MissionState> missionStates = missionService.listMyWeeklyMissionStates(page, Long.parseLong(authentication.getName()), LocalDateTime.now());
+        return WeeklyMissionStateResponseDTO.toDTOPage(missionStates);
+    }
+}
