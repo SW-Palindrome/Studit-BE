655c9bbd57528db2106de9fade49778c63fa4696
Yujeong Jeong <jjungyu1282@gmail.com>
2024-03-27 13:18:13 +0900

    Chore: remove useless imports

--- src/main/java/com/palindrome/studit/domain/study/dao/StudyLogRepository.java
+++ src/main/java/com/palindrome/studit/domain/study/dao/StudyLogRepository.java
@@ -1,13 +1,10 @@
 package com.palindrome.studit.domain.study.dao;
 
-import com.palindrome.studit.domain.study.domain.StudyEnrollment;
 import com.palindrome.studit.domain.study.domain.StudyLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface StudyLogRepository extends JpaRepository<StudyLog, Long> {
-    List<StudyLog> findAllByOrderByCompletedAtDesc();
 }
