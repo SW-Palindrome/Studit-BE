56c02a890137a0a5b2ca46fec321b39e977d3259
Yujeong Jeong <jjungyu1282@gmail.com>
2024-04-03 11:17:58 +0900

    Feat: add velog post variables

--- src/main/java/com/palindrome/studit/domain/study/variable/VelogPostVariables.java
+++ src/main/java/com/palindrome/studit/domain/study/variable/VelogPostVariables.java
@@ -3,12 +3,9 @@ package com.palindrome.studit.domain.study.variable;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Builder
 @Getter
-public class VelogPostsVariables implements Variables{
+public class VelogPostVariables implements Variables{
 
     private String username;
     private String tag;
