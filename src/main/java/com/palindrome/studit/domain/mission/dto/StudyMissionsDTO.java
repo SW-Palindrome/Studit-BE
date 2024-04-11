6501ab52b31c566a9636b042b3d31aa9824f9e75
Yujeong Jeong <jjungyu1282@gmail.com>
2024-04-11 20:17:39 +0900

    Feat: add study missions dto

--- src/main/java/com/palindrome/studit/domain/mission/dto/StudyMissionsDTO.java
+++ src/main/java/com/palindrome/studit/domain/mission/dto/StudyMissionsDTO.java
@@ -2,12 +2,13 @@ package com.palindrome.studit.domain.mission.dto;
 
 import com.palindrome.studit.domain.mission.domain.MissionState;
 import com.palindrome.studit.domain.mission.domain.MissionStatus;
-import com.palindrome.studit.domain.study.dto.StudyDetailDTO;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,32 +22,39 @@ public class StudyMissionsDTO {
     private MissionStatus status;
     private String completedMissionUrl;
 
-    public static Page<StudyDetailDTO.Mission> toDTO(Page<MissionState> missionStates) {
-        List<StudyDetailDTO.Mission> missions = missionStates.stream()
-                .flatMap(missionState -> {
-                    Stream<StudyDetailDTO.Mission> completedMissions = missionState.getMissionLogs().stream()
-                            .map(missionLog -> StudyDetailDTO.Mission.builder()
-                                    .nickname(missionState.getStudyEnrollment().getUser().getNickname())
-                                    .startAt(missionState.getStartAt())
-                                    .endAt(missionState.getEndAt())
-                                    .status(MissionStatus.COMPLETED)
-                                    .completedMissionUrl(missionLog.getCompletedMissionUrl())
-                                    .build());
+    public static List<StudyMissionsDTO> toDTO(Page<MissionState> missionStates) {
+        List<StudyMissionsDTO> allMissions = missionStates.stream().flatMap(missionState -> {
 
-                    Stream<StudyDetailDTO.Mission> incompleteMissions = Stream.generate(() -> StudyDetailDTO.Mission.builder()
-                                    .nickname(missionState.getStudyEnrollment().getUser().getNickname())
-                                    .startAt(missionState.getStartAt())
-                                    .endAt(missionState.getEndAt())
-                                    .status(MissionStatus.INCOMPLETE)
-                                    .completedMissionUrl(null)
-                                    .build())
-                            .limit(missionState.getIncompleteMissionCounts());
+            List<StudyMissionsDTO> completedMissions = missionState.getMissionLogs().stream()
+                    .map(missionLog -> StudyMissionsDTO.builder()
+                            .nickname(missionState.getStudyEnrollment().getUser().getNickname())
+                            .startAt(missionState.getStartAt())
+                            .endAt(missionState.getEndAt())
+                            .status(MissionStatus.COMPLETED)
+                            .completedMissionUrl(missionLog.getCompletedMissionUrl())
+                            .build())
+                    .toList();
 
-                    return Stream.concat(completedMissions, incompleteMissions);
-                })
-                .collect(Collectors.toList());
+            List<StudyMissionsDTO> incompleteMissions = Stream.generate(() -> StudyMissionsDTO.builder()
+                            .nickname(missionState.getStudyEnrollment().getUser().getNickname())
+                            .startAt(missionState.getStartAt())
+                            .endAt(missionState.getEndAt())
+                            .status(MissionStatus.INCOMPLETE)
+                            .completedMissionUrl(null)
+                            .build())
+                    .limit(missionState.getIncompleteMissionCounts())
+                    .toList();
 
-        return missions;
+            List<StudyMissionsDTO> missions = new ArrayList<>();
+            missions.addAll(completedMissions);
+            missions.addAll(incompleteMissions);
+            return missions.stream();
+        }).collect(Collectors.toList());
 
+        return allMissions;
+    }
+
+    public static Page<StudyMissionsDTO> toDTOPage(Page<MissionState> missionStates) {
+        return new PageImpl<>(StudyMissionsDTO.toDTO(missionStates), missionStates.getPageable(), missionStates.getTotalElements());
     }
 }
